e7852f2be4783e40706e47122a4eee06f7247089
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-10-12 04:39:45 +0530

    Refactor1: One Generic Method and find maimum

--- src/main/java/maximumgenerictest/GenericTestMaximum.java
+++ src/main/java/maximumgenerictest/GenericTestMaximum.java
@@ -2,7 +2,26 @@ package maximumgenerictest;
 
 public class GenericTestMaximum 
 {
-	//UC1
+	//UC4
+	public <E extends Comparable<E>> E findMaximum(E a, E b, E c)
+	{
+		E max = a;
+		
+		if(b.compareTo(max) > 0)
+		{
+			max = b;
+		}
+		
+		if(c.compareTo(max) > 0)
+		{
+			max = c;
+		}
+			
+		return max;
+	}
+	
+	
+	/*//UC1
 	public Integer findMaximum(Integer a, Integer b, Integer c) 
 	{
 		Integer max = a;
@@ -54,5 +73,5 @@ public class GenericTestMaximum
 		}
 		
 		return max;
-	}
+	}*/
 }
--- src/test/java/maximumgenerictest/GenericTestMaximumJunitTest.java
+++ src/test/java/maximumgenerictest/GenericTestMaximumJunitTest.java
@@ -8,46 +8,46 @@ public class GenericTestMaximumJunitTest
 {
 
 	//UC1
-	@Test
-	public void givenInteger_returnNumber_whenMaximum() 
-	{
-		GenericTestMaximum objGenericTestMaximum = new GenericTestMaximum();
-		
-		assertSame(3, objGenericTestMaximum.findMaximum(1, 2, 3));
-		assertSame(3, objGenericTestMaximum.findMaximum(1, 3, 2));
-		assertSame(3, objGenericTestMaximum.findMaximum(3, 1, 2));
-	}
+		@Test
+		public void givenInteger_returnNumber_whenMaximum() 
+		{
+			GenericTestMaximum objGenericTestMaximum = new GenericTestMaximum();
+			
+			assertSame(3, objGenericTestMaximum.findMaximum(1, 2, 3));
+			assertSame(3, objGenericTestMaximum.findMaximum(1, 3, 2));
+			assertSame(3, objGenericTestMaximum.findMaximum(3, 1, 2));
+		}
 	
 	//UC2
-	@Test
-	public void givenFirstFloat_returnFirstNumber_whenMaximum() 
-	{
-		GenericTestMaximum objGenericTestMaximum =new GenericTestMaximum();
+		@Test
+		public void givenFirstFloat_returnFirstNumber_whenMaximum() 
+		{
+			GenericTestMaximum objGenericTestMaximum =new GenericTestMaximum();
+			
+			Float actual = objGenericTestMaximum.findMaximum(3f, 2f, 1f);
+			Float expect = 3f;
+			assertEquals(expect, actual);
+		}
 		
-		Float actual = objGenericTestMaximum.findMaximum(3f, 2f, 1f);
-		Float expect = 3f;
-		assertEquals(expect, actual);
-	}
-	
-	@Test
-	public void givenSecondFloat_returnSecondNumber_whenMaximum() 
-	{
-		GenericTestMaximum objGenericTestMaximum =new GenericTestMaximum();
+		@Test
+		public void givenSecondFloat_returnSecondNumber_whenMaximum() 
+		{
+			GenericTestMaximum objGenericTestMaximum =new GenericTestMaximum();
+			
+			Float actual = objGenericTestMaximum.findMaximum(1f, 3f, 2f);
+			Float expect = 3f;
+			assertEquals(expect, actual);
+		}
 		
-		Float actual = objGenericTestMaximum.findMaximum(1f, 3f, 2f);
-		Float expect = 3f;
-		assertEquals(expect, actual);
-	}
-	
-	@Test
-	public void givenThirdFloat_returnThirdNumber_whenMaximum() 
-	{
-		GenericTestMaximum objGenericTestMaximum =new GenericTestMaximum();
-		
-		Float actual = objGenericTestMaximum.findMaximum(1f, 2f, 3f);
-		Float expect = 3f;
-		assertEquals(expect, actual);
-	}
+		@Test
+		public void givenThirdFloat_returnThirdNumber_whenMaximum() 
+		{
+			GenericTestMaximum objGenericTestMaximum =new GenericTestMaximum();
+			
+			Float actual = objGenericTestMaximum.findMaximum(1f, 2f, 3f);
+			Float expect = 3f;
+			assertEquals(expect, actual);
+		}
 
 	//UC3
 		@Test
